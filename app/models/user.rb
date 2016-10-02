13a2c2806dfad441779c2927a14c0ed1cb842a29
Ume <fm189503@gmail.com>
2016-10-02 14:08:22 -0400

    completed devise and user log-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  attr_accessible :email, :username
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
 end
